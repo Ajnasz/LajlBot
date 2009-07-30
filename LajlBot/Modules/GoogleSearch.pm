cf3eccfb8c02cf2cc24c831ae9da38668dcbb254
Lajos Koszti <ajnasz@ajnasz.hu>
2009-07-30 21:33:11 +0200

    coding style changes

--- LajlBot/Modules/GoogleSearch.pm
+++ LajlBot/Modules/GoogleSearch.pm
@@ -5,7 +5,6 @@ package LajlBot::Modules::GoogleSearch;
 use REST::Google::Search;
 use Encode;
 use strict;
-# @ISA('LajlBot::Modules');
 
 
 sub new {
@@ -21,16 +20,19 @@ sub new {
 }
 
 sub action {
-
-  my ($self, $string) = @_;
+  my $self = shift;
+  my ($command, $text, $event, $bot) = @_;
+  if($text eq '') {
+    return 'no given search pattern';
+  }
 
   REST::Google::Search->http_referer('http://google.com');
-  my $google = REST::Google::Search->new(q=>$string, hl=>'en', num=>3);
+  my $google = REST::Google::Search->new(q=>$text, hl=>'en', num=>3);
   die "response status failure" if $google->responseStatus != 200;
   my @results = $google->responseData->results;
   # print Dumper @results;
   my @out;
-  push(@out, 'searching for ' . $string);
+  push(@out, 'searching for ' . $text);
   foreach my $r (@results) {
     # print $r->title;
     push(@out, encode('iso8859-2', $r->titleNoFormatting) . ' -> ' . $r->url);
