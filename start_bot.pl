9e0e5029c71752a08505c375f27e86e429a3a713
Lajos Koszti <ajnasz@ajnasz.hu>
2009-07-30 21:34:32 +0200

    remove port setter

--- start_bot.pl
+++ start_bot.pl
@@ -2,9 +2,9 @@
 
 use LajlBot;
 
+
 print "start\n";
 my $bot = LajlBot->new({config => '/home/ajnasz/.lajlbotconf.ini'});
-$bot->set('port', 6668);
 $bot->connect();
 print "connected\n";
 $bot->start();
